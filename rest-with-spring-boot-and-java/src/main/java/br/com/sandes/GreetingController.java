cd283e539f3c22891897e1771859a90461f09927
Mateus Sandes <mateussandes@hotmail.com.br>
2024-07-06 19:29:17 -0300

    Criado a estrutura inicial do projeto

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/sandes/GreetingController.java
@@ -0,0 +1,24 @@
+package br.com.sandes;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s";
+	private static final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(name = "name",
+						  defaultValue = "World")
+			String name) {
+		
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+}
---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/sandes/StartApp.java
@@ -0,0 +1,13 @@
+package br.com.sandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApp.class, args);
+	}
+
+}
